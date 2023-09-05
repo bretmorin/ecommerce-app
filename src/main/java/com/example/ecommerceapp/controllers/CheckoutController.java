2328ff2ccad8bf782ddcf65ba6b9e1ea992e61ab
Bret Morin <bmorin8@wgu.edu>
2023-09-04 21:34:58 -0400

    Add CheckoutController in controllers package. Add logic for /purchase POST

---
+++ src/main/java/com/example/ecommerceapp/controllers/CheckoutController.java
@@ -0,0 +1,32 @@
+package com.example.ecommerceapp.controllers;
+
+import com.example.ecommerceapp.services.CheckoutService;
+import com.example.ecommerceapp.services.Purchase;
+import com.example.ecommerceapp.services.PurchaseResponse;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+	private CheckoutService checkoutService;
+
+	public CheckoutController(CheckoutService checkoutService) {
+		this.checkoutService = checkoutService;
+	}
+
+	@PostMapping("/purchase")
+	public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+		PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+		return purchaseResponse;
+	}
+
+
+
+
+}
+
+
+
